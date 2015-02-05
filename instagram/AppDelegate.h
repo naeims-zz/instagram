673bd81ff8c7290cc3bc4424005ff74c28e80cd2
Naeim Semsarilar <nsemsarilar@box.com>
2015-02-04 19:27:01 -0800

    Initial Commit

---
+++ instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instagram
+//
+//  Created by Naeim Semsarilar on 2/4/15.
+//  Copyright (c) 2015 naeim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  instagram
+//
+//  Created by Naeim Semsarilar on 2/4/15.
+//  Copyright (c) 2015 naeim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
